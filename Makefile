1741ae03cdd648f7c95920c77fdd8b56bad7565b
Linus Zipperer <linus@iMac-von-Linus.local>
2025-11-15 22:56:52 +0100

    Neue Ordnerstruktur und Anfang in term.c/.h

--- Makefile
+++ Makefile
@@ -1,56 +1,6 @@
-#Compiler und Flags
-CC := clang
-CFLAGS := -Wall -Wextra -O2 -g
-LDFLAGS :=
+CC = cc
 
-BINDIR := bin
-BUILDDIR := build
+CFLAGS = -Wall -Wextra -Werror -g
 
-SRCS_MAIN := $(wildcard main/*.c)
-SRCS_INPUT := $(wildcard input/*.c)
-SRCS_TERM := $(wildcard term/*.c)
-
-OBJS_MAIN := $(patsubst %.c,$(BUILDDIR)/main/%.o,$(notdir $(SRCS_MAIN)))
-OBJS_INPUT := $(patsubst %.c,$(BUILDDIR)/input/%.o,$(notdir $(SRCS_INPUT)))
-OBJS_TERM := $(patsubst %.c,$(BUILDDIR)/term/%.o,$(notdir $(SRCS_TERM)))
-
-OBJS := $(OBJS_MAIN) $(OBJS_INPUT) $(OBJS_TERM)
-
-.PHONY: all clean main input term
-
-all: $(BINDIR)/lert
-
-$(BINDIR)/lert: $(OBJS) | $(BINDIR)
-	$(CC) $(LDFLAGS) -o $@ $^
-
-# Per-module targets (create static libs for each module)
-main: $(BUILDDIR)/libmain.a
-input: $(BUILDDIR)/libinput.a
-term: $(BUILDDIR)/libterm.a
-
-$(BUILDDIR)/libmain.a: $(OBJS_MAIN) | $(BUILDDIR)/main
-	ar rcs $@ $^
-
-$(BUILDDIR)/libinput.a: $(OBJS_INPUT) | $(BUILDDIR)/input
-	ar rcs $@ $^
-
-$(BUILDDIR)/libterm.a: $(OBJS_TERM) | $(BUILDDIR)/term
-	ar rcs $@ $^
-
-# Compile rule: build/<module>/<file>.o from <module>/<file>.c
-$(BUILDDIR)/%/%.o: %/%.c | $(BUILDDIR)/%
-	$(CC) $(CFLAGS) -c -o $@ $<
-
-# Directories
-$(BINDIR):
-	mkdir -p $@
-
-$(BUILDDIR):
-	mkdir -p $@
-
-$(BUILDDIR)/%:
-	mkdir -p $@
-
-clean:
-	rm -rf $(BUILDDIR) $(BINDIR)
-
+all: term.c term.h
+	$(CC) $(CFLAGS) -c term.c -o lert
---
+++ term.c
@@ -0,0 +1,101 @@
+#include "term.h"
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct termios BACKUP_TTY;
+
+wchar_t s[3][11] = {
+                    {L'q', L'w', L'e', L'r', L't', L'z', L'u', L'i', L'o', L'p', L'ü'},
+                    {L'a', L's', L'd', L'f', L'g', L'h', L'j', L'k', L'l', L'ö', L'ä'},
+                    {L'y', L'x', L'c', L'v', L'b', L'n', L'm', L',', L'.', L'-', L' '}
+                   };
+
+
+
+int rndChar(char *s) {
+  if (!s) {
+    return 0;
+  }
+
+  int x = rand() % 3;
+  switch: x
+    case: 0
+
+  return 1;
+}
+
+
+int new_tty(int fd) {
+
+  struct termios buff;
+  /* Wir fragen nach den Attributen des Terminals und */
+  /* übergeben diese dann an buffer. BACKUP_TTY dient   */
+  /* bei Programmende zur Wiederherstellung der alten   */
+  /* Attribute und bleibt somit unberührt.              */
+  if ((tcgetattr (fd, &BACKUP_TTY)) == -1)
+    return -1;
+  buff = BACKUP_TTY;
+  /* Lokale Flags werden gelöscht :                   */
+  /* ECHO - Zeichenausgabe auf Bildschirm             */
+  /* ICANON - Zeilenorientierter Eingabemodus         */
+  /* ISIG – Terminal-Steuerzeichen (kein STRG+C mehr  */
+  /* möglich)                                         */
+  buff.c_lflag &= ~(ECHO | ICANON | ISIG);
+  /* VMIN = Anzahl der Bytes die gelesen werden bevor read abbricht */
+  /* VMIN = 1*/
+  buff.c_cc[VMIN] = 1;
+
+  if (tcsetattr(fd, TCSAFLUSH, &buff) == -1) {
+    return -1;
+  }
+
+  return 0;
+}
+
+
+/* Hole die Größe des Terminals.
+   TODO: Das Richtige Terminal muss noch ermittelt werden,
+         also ersetze die 1 in ioctl.
+*/
+void tc_get_size(int* rows, int *cols) {
+  struct winsize size;
+  ioctl(1, TIOCGWINSZ, &size);
+  *rows = size.ws_row;
+  *cols = size.ws_col;
+}
+
+
+static int restore_tty(int fd) {
+  if (tcsetattr(fd, TCSAFLUSH, &BACKUP_TTY) == -1) {
+    return -1;
+  }
+  return 0;
+}
+
+
+int main() {
+
+  int rows = 0;
+  int cols = 0;
+  // clear_screen();
+  tc_enable_alt_buff();
+  clear_screen();
+  puts(TC_RED);
+  tc_get_size(&rows, &cols);
+  int rows_2 = rows /2;
+  tc_move_cursor((cols -39)/2, rows_2 - 2);
+  printf("söjk kgad ksjä ököa löfö ddsa kjfs äglj\n\n");
+  tc_move_cursor((cols -39)/2, rows_2);
+  puts("Dies tippt der User");
+  while (!getchar())   {
+    
+  }  
+  puts(TC_RESET);
+  tc_disable_alt_buff();
+  return EXIT_SUCCESS;
+}
+
